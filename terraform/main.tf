e2229f4736926bd3c3b73a06955266d99ff915e3
Akki-8989 <akashpatil6269@gmail.com>
2026-01-07 23:43:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,31 +9,11 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-}
-
-# Variables
-variable "subscription_id" {
-  type        = string
-  description = "Azure Subscription ID"
 }
 
 variable "app_name" {
   type        = string
-  description = "Application name for resource naming"
-}
-
-variable "create_sql_server" {
-  type        = bool
-  default     = false
-  description = "Whether to create SQL Server and Database"
-}
-
-variable "sql_admin_password" {
-  type        = string
-  default     = ""
-  sensitive   = true
-  description = "SQL Server admin password (required if create_sql_server is true)"
+  description = "Application name (used for resource naming)"
 }
 
 variable "location" {
@@ -41,36 +21,44 @@ variable "location" {
   default = "Central India"
 }
 
-# Local variables for naming
+variable "sql_admin_password" {
+  type        = string
+  description = "SQL Server admin password (optional - if not provided, SQL Server won't be created)"
+  default     = ""
+  sensitive   = true
+}
+
 locals {
-  resource_name = lower(replace(var.app_name, ".", "-"))
+  resource_prefix    = replace(lower(var.app_name), ".", "-")
+  create_sql_server  = var.sql_admin_password != ""
 }
 
 # Resource Group
 resource "azurerm_resource_group" "main" {
-  name     = "${local.resource_name}-rg"
+  name     = "${local.resource_prefix}-rg"
   location = var.location
 }
 
 # App Service Plan
 resource "azurerm_service_plan" "main" {
-  name                = "${local.resource_name}-plan"
+  name                = "${local.resource_prefix}-plan"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   os_type             = "Windows"
   sku_name            = "F1"
+
+  depends_on = [azurerm_resource_group.main]
 }
 
-# Web App
+# Windows Web App
 resource "azurerm_windows_web_app" "main" {
-  name                = "${local.resource_name}-webapp"
+  name                = "${local.resource_prefix}-webapp"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   service_plan_id     = azurerm_service_plan.main.id
 
   site_config {
-    always_on = false
-
+    always_on = false  # F1 tier doesn't support always_on
     application_stack {
       dotnet_version = "v8.0"
     }
@@ -79,37 +67,57 @@ resource "azurerm_windows_web_app" "main" {
   app_settings = {
     "ASPNETCORE_ENVIRONMENT" = "Production"
   }
+
+  depends_on = [
+    azurerm_resource_group.main,
+    azurerm_service_plan.main
+  ]
 }
 
-# SQL Server (Conditional)
+# SQL Server (conditional)
 resource "azurerm_mssql_server" "main" {
-  count                        = var.create_sql_server ? 1 : 0
-  name                         = "${local.resource_name}-sqlserver"
+  count                        = local.create_sql_server ? 1 : 0
+  name                         = "${local.resource_prefix}-sqlserver"
   resource_group_name          = azurerm_resource_group.main.name
   location                     = azurerm_resource_group.main.location
   version                      = "12.0"
   administrator_login          = "sqladmin"
   administrator_login_password = var.sql_admin_password
+
+  depends_on = [azurerm_resource_group.main]
 }
 
-# SQL Database (Conditional)
+# SQL Database (conditional)
 resource "azurerm_mssql_database" "main" {
-  count     = var.create_sql_server ? 1 : 0
-  name      = "${local.resource_name}-db"
+  count     = local.create_sql_server ? 1 : 0
+  name      = "${local.resource_prefix}-db"
   server_id = azurerm_mssql_server.main[0].id
   sku_name  = "Basic"
+
+  depends_on = [
+    azurerm_resource_group.main,
+    azurerm_mssql_server.main
+  ]
 }
 
-# SQL Firewall Rule - Allow Azure Services (Conditional)
+# SQL Server Firewall Rule - Allow Azure Services (conditional)
 resource "azurerm_mssql_firewall_rule" "allow_azure" {
-  count            = var.create_sql_server ? 1 : 0
+  count            = local.create_sql_server ? 1 : 0
   name             = "AllowAzureServices"
   server_id        = azurerm_mssql_server.main[0].id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
+
+  depends_on = [
+    azurerm_mssql_server.main
+  ]
 }
 
 # Outputs
+output "resource_group" {
+  value = azurerm_resource_group.main.name
+}
+
 output "webapp_name" {
   value = azurerm_windows_web_app.main.name
 }
@@ -118,20 +126,10 @@ output "webapp_url" {
   value = "https://${azurerm_windows_web_app.main.default_hostname}"
 }
 
-output "resource_group" {
-  value = azurerm_resource_group.main.name
-}
-
-output "sql_created" {
-  value = var.create_sql_server
-}
-
 output "sql_server_fqdn" {
-  description = "SQL Server fully qualified domain name"
-  value       = var.create_sql_server ? azurerm_mssql_server.main[0].fully_qualified_domain_name : ""
+  value = local.create_sql_server ? azurerm_mssql_server.main[0].fully_qualified_domain_name : ""
 }
 
 output "sql_database_name" {
-  description = "SQL Database name"
-  value       = var.create_sql_server ? azurerm_mssql_database.main[0].name : ""
+  value = local.create_sql_server ? azurerm_mssql_database.main[0].name : ""
 }
