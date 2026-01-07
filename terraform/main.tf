c5247c391bc6cc4c6ea93e27b29c727acb3d7bc4
Akki-8989 <akashpatil6269@gmail.com>
2026-01-07 17:45:43 +0530

    Refactor Azure Terraform configuration
    Refactor variables and resources for Azure deployment, adding conditionals for SQL Server and database creation.

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,101 +2,66 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
 
 # Variables
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
 
-variable "resource_group_name" {
-  default = "azure-deploy-rg"
+variable "create_sql_server" {
+  type        = bool
+  default     = false
+  description = "Whether to create SQL Server and Database"
+}
+
+variable "sql_admin_password" {
+  type        = string
+  default     = ""
+  sensitive   = true
+  description = "SQL Server admin password (required if create_sql_server is true)"
 }
 
 variable "location" {
-  default = "centralindia"
+  type    = string
+  default = "East US"
 }
 
-variable "sql_server_name" {
-  default = "azure-deploy-sql"
-}
-
-variable "sql_database_name" {
-  default = "azure-deploy-db"
-}
-
-variable "sql_admin_login" {
-  default = "sqladmin"
-}
-
-variable "sql_admin_password" {}
-
-variable "app_service_plan_name" {
-  default = "azure-deploy-plan"
-}
-
-variable "web_app_name" {
-  default = "azure-deploy-webapp"
+# Local variables for naming
+locals {
+  repo_name     = lower(replace(basename(path.cwd), ".", "-"))
+  resource_name = "${local.repo_name}"
 }
 
 # Resource Group
 resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
+  name     = "${local.resource_name}-rg"
   location = var.location
-}
-
-# SQL Server
-resource "azurerm_mssql_server" "main" {
-  name                         = var.sql_server_name
-  resource_group_name          = azurerm_resource_group.main.name
-  location                     = azurerm_resource_group.main.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_login
-  administrator_login_password = var.sql_admin_password
-}
-
-# SQL Database
-resource "azurerm_mssql_database" "main" {
-  name      = var.sql_database_name
-  server_id = azurerm_mssql_server.main.id
-  sku_name  = "Basic"
-}
-
-# Firewall Rule - Allow Azure Services
-resource "azurerm_mssql_firewall_rule" "allow_azure" {
-  name             = "AllowAzureServices"
-  server_id        = azurerm_mssql_server.main.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
 }
 
 # App Service Plan
 resource "azurerm_service_plan" "main" {
-  name                = var.app_service_plan_name
-  resource_group_name = azurerm_resource_group.main.name
+  name                = "${local.resource_name}-plan"
   location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   os_type             = "Windows"
   sku_name            = "B1"
 }
 
 # Web App
 resource "azurerm_windows_web_app" "main" {
-  name                = var.web_app_name
-  resource_group_name = azurerm_resource_group.main.name
+  name                = "${local.resource_name}-webapp"
   location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   service_plan_id     = azurerm_service_plan.main.id
 
   site_config {
@@ -105,31 +70,53 @@ resource "azurerm_windows_web_app" "main" {
     }
   }
 
-  connection_string {
-    name  = "DefaultConnection"
-    type  = "SQLAzure"
-    value = "Server=tcp:${azurerm_mssql_server.main.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.main.name};User ID=${var.sql_admin_login};Password=${var.sql_admin_password};Encrypt=true;Connection Timeout=30;"
+  app_settings = {
+    "ASPNETCORE_ENVIRONMENT" = "Production"
   }
 }
 
+# SQL Server (Conditional)
+resource "azurerm_mssql_server" "main" {
+  count                        = var.create_sql_server ? 1 : 0
+  name                         = "${local.resource_name}-sqlserver"
+  resource_group_name          = azurerm_resource_group.main.name
+  location                     = azurerm_resource_group.main.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = var.sql_admin_password
+}
+
+# SQL Database (Conditional)
+resource "azurerm_mssql_database" "main" {
+  count     = var.create_sql_server ? 1 : 0
+  name      = "${local.resource_name}-db"
+  server_id = azurerm_mssql_server.main[0].id
+  sku_name  = "Basic"
+}
+
+# SQL Firewall Rule - Allow Azure Services (Conditional)
+resource "azurerm_mssql_firewall_rule" "allow_azure" {
+  count            = var.create_sql_server ? 1 : 0
+  name             = "AllowAzureServices"
+  server_id        = azurerm_mssql_server.main[0].id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
 # Outputs
-output "resource_group_name" {
-  value = azurerm_resource_group.main.name
-}
-
-output "web_app_url" {
-  value = "https://${azurerm_windows_web_app.main.default_hostname}"
-}
-
-output "web_app_name" {
+output "webapp_name" {
   value = azurerm_windows_web_app.main.name
 }
 
-output "sql_server_fqdn" {
-  value = azurerm_mssql_server.main.fully_qualified_domain_name
+output "webapp_url" {
+  value = "https://${azurerm_windows_web_app.main.default_hostname}"
 }
 
-output "connection_string" {
-  value     = "Server=tcp:${azurerm_mssql_server.main.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.main.name};User ID=${var.sql_admin_login};Password=${var.sql_admin_password};Encrypt=true;Connection Timeout=30;"
+output "sql_created" {
+  value = var.create_sql_server
+}
+
+output "sql_connection_string" {
+  value = var.create_sql_server ? "Server=tcp:${azurerm_mssql_server.main[0].fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.main[0].name};Persist Security Info=False;User ID=sqladmin;Password=${var.sql_admin_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" : ""
   sensitive = true
 }
